d064fbf7b37aad52ce910c6e0e3fd777e5b0340e
Oles Pisarenko <doctornkz@ya.ru>
2017-12-03 02:22:01 +0300

    Console fix

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup
 setup(name="calc",
       entry_points={
           'console_scripts': [
-              'calc = calc:main', ]},
-      version="0.0.5",
+              'calc = calc.calc:main', ]},
+      version="0.0.6",
       description="A Simple Tkinter Calc",
       author="Oles Pisarenko",
       author_email="doctor.mailbox@mail.ru",
